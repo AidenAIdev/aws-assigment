a08a825923c96e95116fc5a5ff8a5b511fdafa8b
KingP1g <aiden.decastro@epn.edu.ec>
2024-04-05 17:27:32 -0500

    feat: initialize application and create reusable components - navigation on the full page already structured on navbar
    Initialize the application with basic structure and dependencies.
    
    Created:
    - Navbar reusable component with full responsiveness and modern design.
    - Custom Button reusable component with gradient border.
    - Created constants and assets for fully customization with no need to change the codeand modularization
    
    Also:
    - Initialized assets folder to store project assets provided by client.
    Refs: #1

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+import ButtonGradient from "../assets/svg/ButtonGradient";
+
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses = "relative z-10";
+
+  const renderButton = () => (
+    <button className={classes} onClick={onClick}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+<span aria-hidden class="absolute inset-0 z-0 scale-x-[2.0] blur before:absolute before:inset-0 before:top-1/2 before:aspect-square before:animate-disco before:bg-gradient-conic before:from-purple-700 before:via-red-500 before:to-amber-400" />
+    
+  return href ? renderLink() : renderButton();
+};
+
+
+export default Button;
---
+++ src/constants/index.js
@@ -0,0 +1,249 @@
+import {
+    benefitIcon1,
+    benefitIcon2,
+    benefitIcon3,
+    benefitIcon4,
+    benefitImage2,
+    chromecast,
+    disc02,
+    discord,
+    discordBlack,
+    facebook,
+    figma,
+    file02,
+    framer,
+    homeSmile,
+    instagram,
+    notification2,
+    notification3,
+    notification4,
+    notion,
+    photoshop,
+    plusSquare,
+    protopie,
+    raindrop,
+    recording01,
+    recording03,
+    roadmap1,
+    roadmap2,
+    roadmap3,
+    roadmap4,
+    searchMd,
+    slack,
+    sliders04,
+    telegram,
+    twitter,
+    yourlogo,
+  } from "../assets";
+  
+  export const navigation = [
+    {
+      id: "0",
+      title: "Features",
+      url: "#features",
+    },
+    {
+      id: "1",
+      title: "Cómo usar",
+      url: "#how-to-use",
+    },
+    {
+      id: "2",
+      title: "Precios",
+      url: "#pricing",
+    },
+    {
+      id: "3",
+      title: "Roadmap",
+      url: "#roadmap",
+    },
+    {
+      id: "4",
+      title: "Nueva cuenta",
+      url: "#signup",
+      onlyMobile: true,
+    },
+    {
+      id: "5",
+      title: "Ingresa",
+      url: "#login",
+      onlyMobile: true,
+    },
+  ];
+
+  export const aiagencyServices = [
+    "Generación de fotos",
+    "Mejora de fotos",
+    "Integración perfecta",
+  ];
+  
+  export const roadmap = [
+    {
+      id: "0",
+      title: "Investigación de Público Objetivo",
+      text: "Conoce a tu audiencia mediante una investigación profunda basada en datos demográficos, intereses, género y edad, para identificar el target objetivo de tu campaña de marketing.",
+      date: "Noviembre 2023",
+      status: "completado",
+      imageUrl: roadmap1,
+      colorful: true,
+    },
+    {
+      id: "1",
+      title: "Estrategia Creativa",
+      text: "Genera una estrategia única para tu campaña de marketing, incluyendo el 'reason why', objetivos, tono y estilo, todo impulsado por la inteligencia artificial para cautivar a tu audiencia.",
+      date: "Noviembre 2023",
+      status: "en progreso",
+      imageUrl: roadmap2,
+    },
+    {
+      id: "2",
+      title: "Ideas Creativas",
+      text: "Obtén ideas innovadoras para tu campaña, adaptadas a diferentes medios de publicación, desde videos en YouTube hasta anuncios de audio en plataformas como Spotify, todo gracias a la inteligencia artificial.",
+      date: "Diciembre 2023",
+      status: "completado",
+      imageUrl: roadmap3,
+    },
+    {
+      id: "3",
+      title: "Creación de Anuncios de Audio",
+      text: "Potencia tu marca con anuncios de audio impactantes, como los audios publicitarios de radio o Spotify, creados para destacar y llegar a tu audiencia de manera efectiva.",
+      date: "Enero 2024",
+      status: "en progreso",
+      imageUrl: roadmap4,
+    },
+  ];
+  
+    export const collabText =
+    "Con nuestra plataforma de creación de campañas publicitarias y herramientas avanzadas, ofrecemos una solución integral para impulsar la creatividad y el éxito en tus proyectos de marketing.";
+
+  
+    export const pricing = [
+      {
+        id: "0",
+        title: "Básico",
+        description: "Desbloquea las funcionalidades esenciales para lanzar tu campaña publicitaria con estilo.",
+        price: "$9.99",
+        features: [
+          "Crea tu marca y campaña publicitaria",
+          "Investigación de Público Objetivo",
+          "Estrategia Creativa",
+          "Ideas Creativas",
+          "Creación de Anuncios de Audio",
+          "Obtén 100 créditos gratis",
+        ],
+      },
+      {
+        id: "1",
+        title: "Premium",
+        description: "Lleva tu campaña al siguiente nivel con acceso completo a todas las herramientas avanzadas y soporte prioritario.",
+        price: "$19.99",
+        features: [
+          "Todas las características del plan Básico",
+          "Análisis Avanzado de Campañas",
+          "Soporte Prioritario",
+          "Actualizaciones Exclusivas",
+          "Obtén 100 créditos gratis",
+        ],
+      },
+      {
+        id: "2",
+        title: "Empresa",
+        description: "Potencia tu negocio con funcionalidades personalizadas y soporte dedicado.",
+        price: "Personalizado",
+        features: [
+          "Funcionalidades personalizadas",
+          "Soporte Dedicado",
+          "Integración con Herramientas Empresariales",
+          "Gestión de Usuarios",
+          "Obtén 300 créditos gratis",
+        ],
+      },
+    ];
+    
+  export const benefits = [
+    {
+      id: "0",
+      title: "Marca Personalizada",
+      text: "Crea una marca única agregando información relevante y el logo de tu campaña publicitaria, junto con su objetivo, todo en un solo lugar.",
+      backgroundUrl: "./src/assets/benefits/card-1.svg",
+      iconUrl: benefitIcon1,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "1",
+      title: "Inteligencia de Audiencia",
+      text: "Descubre a tu audiencia ideal mediante una investigación profunda que analiza datos demográficos, intereses y comportamientos, proporcionando un target objetivo preciso para tu campaña.",
+      backgroundUrl: "./src/assets/benefits/card-2.svg",
+      iconUrl: benefitIcon2,
+      imageUrl: benefitImage2,
+      light: true,
+    },
+    {
+      id: "2",
+      title: "Estrategia Creativa",
+      text: "Desata tu creatividad con una estrategia única generada por IA que incluye el 'reason why', objetivos, tono y estilo, para cautivar a tu audiencia de manera impactante.",
+      backgroundUrl: "./src/assets/benefits/card-3.svg",
+      iconUrl: benefitIcon3,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "3",
+      title: "Inspiración Ilimitada",
+      text: "Obtén ideas frescas y originales para tu campaña adaptadas a cada medio de publicación, desde videos en YouTube hasta anuncios en redes sociales, todo alimentado por IA.",
+      backgroundUrl: "./src/assets/benefits/card-4.svg",
+      iconUrl: benefitIcon4,
+      imageUrl: benefitImage2,
+      light: true,
+    },
+    {
+      id: "4",
+      title: "Planificación Eficaz",
+      text: "Optimiza tu presupuesto con una tabla detallada de planificación que sugiere los mejores medios para tu campaña, todo respaldado por la inteligencia artificial.",
+      backgroundUrl: "./src/assets/benefits/card-5.svg",
+      iconUrl: benefitIcon1,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "5",
+      title: "Creatividad Sin Límites",
+      text: "Deja volar tu imaginación con la generación de imágenes publicitarias y textos complementarios creados por IA, listos para impactar en Instagram, Google y más.",
+      backgroundUrl: "./src/assets/benefits/card-6.svg",
+      iconUrl: benefitIcon2,
+      imageUrl: benefitImage2,
+    },
+  ];
+  
+  
+  export const socials = [
+    {
+      id: "0",
+      title: "Discord",
+      iconUrl: discordBlack,
+      url: "#",
+    },
+    {
+      id: "1",
+      title: "Twitter",
+      iconUrl: twitter,
+      url: "#",
+    },
+    {
+      id: "2",
+      title: "Instagram",
+      iconUrl: instagram,
+      url: "#",
+    },
+    {
+      id: "3",
+      title: "Telegram",
+      iconUrl: telegram,
+      url: "#",
+    },
+    {
+      id: "4",
+      title: "Facebook",
+      iconUrl: facebook,
+      url: "#",
+    },
+  ];
+  
