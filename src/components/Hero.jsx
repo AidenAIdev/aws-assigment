3163fddf5539a1a10d0ed3c667bd953587f5dbe5
KingP1g <aiden.decastro@epn.edu.ec>
2024-04-06 00:04:11 -0500

    feat: add image to hero section and style aspects
    Added an image to the hero section to enhance visual appeal and convey key content effectively.
    
    Styled aspects of the hero section, including alignment, sizing, and typography, to ensure a cohesive and visually appealing layout.
    
    Refs: #3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import Section from './Section'
 import React from 'react'
-import { curve } from '../assets'
+import { aiagencySymbol, robot, curve } from '../assets'
 import Button from './Button'
 function Hero() {
   return (
@@ -18,19 +18,32 @@ function Hero() {
             className='relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]'
             >
                 <h1 className='h1 mb-6'>
-                    Explora las posibilidades del marketing con Inteligencia Artificial
-                    <hr/>
+                    Explora las nuevas posibilidades del marketing con {"  "}
                     <span className='inline-block relative'>
                         AiAGENCY
-                        <img src={curve} className='absolute top-full left-0 w-full xl:mt-2' width={624} height={28} alt='curve'/>
+                        <img src={curve} className='absolute  left-0 w-full xl:mt-1' width={624} height={28} alt='curve'/>
 
                     </span>
                 </h1>
                 <p className='body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8'>
                     Crea todo lo que te imaginas y mas con AI agency
                 </p>
-                <Button>Subscribete Ya</Button>
+                <Button href='/subscribe' white
+                >Subscribete Ya</Button>
             </div>
+            <div className='relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24'>
+                <div className='relative z-1 p-0.5 rounded-2xl bg-conic-gradient'>
+                    <div className='relative bg-n-8 rounded-[1rem]'>
+                        <div className='h-[1.4rem] bg-n-10 rounded-t-[0.9rem]'/>
+                        <div>
+                            <img src={robot} className='w-full' width={400} height={400} alt='robot'/>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     </Section>
   )
